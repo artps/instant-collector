490da4a08460cf0e900a56df1cbca9ac7e4fad26
Artyom Semyonov <sevenov@gmail.com>
2012-08-24 04:45:36 +0400

    Added ets initialization

--- src/collector_app.erl
+++ src/collector_app.erl
@@ -5,12 +5,23 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(TABLE, broadcast_table).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    ensure_ets_exists(),
     collector_sup:start_link().
 
 stop(_State) ->
     ok.
+
+ensure_ets_exists() ->
+    case ets:info(?TABLE, memory) of
+        undefined ->
+            ets:new(?TABLE, [public, named_table]);
+        _Any ->
+            ok
+    end.
